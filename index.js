3e44ec9728a1ef625ceca75cd38d95a1a28d56b8
Shahid Mulani <shahid.mulani@inmobi.com>
2025-09-16 12:44:15 +0530

    add eslint and prettier

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Listening on ${PORT} ...`);
-})
-
+  console.log(`Listening on ${PORT} ...`);
+});
--- src/app.js
+++ src/app.js
@@ -2,9 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req,res) => {
-    req.status(200).send('Hello from acquisitions');
-})
+app.get('/', (req, res) => {
+  req.status(200).send('Hello from acquisitions');
+});
 
 export default app;
-
