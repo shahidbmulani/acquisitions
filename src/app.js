1c5ff5b14bc23306082ad9a4f77fb4de50888f48
Shahid Mulani <shahid.mulani@inmobi.com>
2025-09-16 18:31:13 +0530

    db setup in progress

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import logger from './config/logger.js';
 
 const app = express();
 
 app.get('/', (req, res) => {
+    logger.info('hello from logger');
   req.status(200).send('Hello from acquisitions');
 });
 
